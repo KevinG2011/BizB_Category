9dd12804be502882994d6fedbc86488666ce4c85
lijia <lijia@huajiao.tv>
2019-02-01 15:45:28 +0800

    init project

---
+++ BizB_Category/BizB_Category.m
@@ -0,0 +1,13 @@
+//
+//  BizB_Category.m
+//  BizB_Category
+//
+//  Created by lijia on 2019/2/1.
+//  Copyright © 2019 MJHF. All rights reserved.
+//
+
+#import "BizB_Category.h"
+
+@implementation BizB_Category
+
+@end
